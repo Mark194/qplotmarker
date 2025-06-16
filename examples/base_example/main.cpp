5cfe9f8d1ca708230397c7bc9e55dc1308bb9d00
zhenya15.markov@gmail.com <zhenya15.markov@gmail.com>
2025-06-16 09:58:40 +0300

    Refactor: code style has been updated

--- examples/base_example/main.cpp
+++ examples/base_example/main.cpp
@@ -10,41 +10,30 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    // // Создаем график и серию данных
-    QChart *chart = new QChart();
-    QLineSeries *series = new QLineSeries();
+    auto *chart = new QChart();
+    auto *series = new QLineSeries();
 
-    // // Добавляем данные в серию
-    *series << QPointF(1, 1) << QPointF(2, 3) << QPointF(3, 2) << QPointF(4, 4) << QPointF(5, 3);
-
-    // // Настраиваем график
+    *series << QPointF(1, 1) << QPointF(2, 3) << QPointF(3, 2) << QPointF(4, 4)
+            << QPointF(5, 3);
+    
     chart->addSeries(series);
     chart->createDefaultAxes();
     chart->legend()->setVisible(false);
-    // qDebug() << "X)";
 
-
-    QChartView *chartView = new QChartView(chart);
+    auto *chartView = new QChartView(chart);
     chartView->setRenderHint(QPainter::Antialiasing);
     chartView->resize(800, 600);
-    chartView->setWindowTitle("Пример использования QPlotMarker");
+    chartView->setWindowTitle("Example of using QPlotMarker");
     chartView->show();
 
-    // // Создаем маркер
-    QPlotMarker * oneMarker = new QPlotMarker(chart, Qt::red, Qt::Horizontal);
+    auto * oneMarker = new QPlotMarker(chart, Qt::red, Qt::Horizontal);
+    oneMarker->setLabelFormat("%.2f");
 
-    oneMarker->setLabelFormat("%.2f"); // Формат отображаемых координат
-
-
-    QPlotMarker *otherMarker = new QPlotMarker(chart, Qt::red, Qt::Vertical);
-
-    otherMarker->setLabelFormat("%.2f"); // Формат отображаемых координат
-
+    auto *otherMarker = new QPlotMarker(chart, Qt::red, Qt::Vertical);
+    otherMarker->setLabelFormat("%.2f");
 
     chartView->scene()->addItem( oneMarker );
-
     chartView->scene()->addItem( otherMarker );
-
 
     return a.exec();
 }
